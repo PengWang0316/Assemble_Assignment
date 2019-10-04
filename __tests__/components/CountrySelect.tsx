12c2e1fbaa199712d03b729e7e96fe5117029547
Kevin Wang <rocwang0316@gmail.com>
2019-10-04 14:59:41 -0700

    Added the test code for the CountrySelect component.

--- __tests__/components/CountrySelect.tsx
+++ __tests__/components/CountrySelect.tsx
@@ -2,35 +2,35 @@ import React from 'react';
 import { shallow, ShallowWrapper } from 'enzyme';
 import renderer from 'react-test-renderer';
 
-import { StateSelect } from '../../app/components/StateSelect/StateSelect';
+import { CountrySelect } from '../../app/components/CountrySelect/CountrySelect';
 
-describe('Test the StateSelect componnet', () => {
+describe('Test the CountrySelect component', () => {
   let component: ShallowWrapper;
 
   beforeAll(() => {
-    component = shallow(<StateSelect />);
+    component = shallow(<CountrySelect />);
   });
 
   test('should have one label', () => {
     expect(component.find('label').length).toBe(1);
   });
 
-  test('should have one span with text State', () => {
+  test('should have one span with text Country', () => {
     const span = component.find('span');
     expect(span.length).toBe(1);
-    expect(span.first().text()).toBe('State');
+    expect(span.first().text()).toBe('Country');
   });
 
   test('should have one select', () => {
     expect(component.find('select').length).toBe(1);
   });
 
-  test('should have 52 options', () => {
+  test('should have 250 options', () => {
     const options = component.find('option');
-    expect(options.length).toBe(52);
-    expect(options.first().text()).toBe('State');
+    expect(options.length).toBe(250);
+    expect(options.first().text()).toBe('Country');
     expect(options.first().prop('value')).toBe('--');
   });
 
-  test('snapshot should be the same', () => expect(renderer.create(<StateSelect />).toJSON()).toMatchSnapshot());
+  test('snapshot should be the same', () => expect(renderer.create(<CountrySelect />).toJSON()).toMatchSnapshot());
 });
